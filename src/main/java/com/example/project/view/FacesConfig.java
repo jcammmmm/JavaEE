2c590501384458f6f2024c7b5fdcfbc31e737a9b
Juan Camilo <jcflorezv@unal.edu.co>
2019-07-22 09:00:25 -0500

    BASE

---
+++ src/main/java/com/example/project/view/FacesConfig.java
@@ -0,0 +1,7 @@
+package com.example.project.view;
+
+@javax.faces.annotation.FacesConfig
+public class FacesConfig  {
+
+
+}
--- src/main/java/com/example/project/view/HelloWorld2.java
+++ src/main/java/com/example/project/view/HelloWorld2.java
@@ -3,14 +3,13 @@ package com.example.project.view;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 //@FacesConfig // FIX javax.el.PropertyNotFoundException: /hello.xhtml @14,67 value="#{helloWorld.input}": Target Unreachable, identifier 'helloWorld' resolved to null
-@Named("helloWorld") // gives the bean a name, which is primarily used to reference it via EL
+@Named // gives the bean a name, which is primarily used to reference it via EL
 @RequestScoped // lifespan is the duration of an HTTP request
-public class HelloWorld {
+public class HelloWorld2 {
 	private String input;
 	private String output = "HW.output_default_string";
 	
 	public void submit() {
-		
 		output = "Hello World! You have typed: " + input;
 		for(int i = 0; i < 10; i++) {
 			double d = i*i;
