8e12220f657a95e59bce679a10de43bdbe87d2ee
Juan Camilo <jcflorezv@unal.edu.co>
2019-07-23 13:18:48 -0500

    crud working...

--- src/main/java/com/example/project/view/Country.java
+++ src/main/java/com/example/project/view/Country.java
@@ -1,14 +1,29 @@
 package com.example.project.view;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Country {
+// It is only a plain object, not a functional bean.
+public class Country implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private long id;
 	private String name;
 	private int population;
 	private double extension;
 	private GovType govType;
 	private ArrayList<War> activeWars;
 	
-	public Country(String name, int population, double extension, GovType govType, ArrayList<War> activeWars) {
+	public Country() {
+		id = DisplayCountries.getNextId();
+		name = "";
+		population = 0;
+		extension = 0.0;
+		govType = GovType.Anarchy;
+		activeWars = new ArrayList<War>();
+	}
+	
+	public Country(long id, String name, int population, double extension, GovType govType, ArrayList<War> activeWars) {
+		super();
+		this.id = id;
 		this.name = name;
 		this.population = population;
 		this.extension = extension;
@@ -16,43 +31,25 @@ public class Country {
 		this.activeWars = activeWars;
 	}
 
-	public String getName() {
-		return name;
-	}
+	public long getId() { return id; }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+	public String getName() { return name; }
 
-	public int getPopulation() {
-		return population;
-	}
+	public void setName(String name) { this.name = name; }
 
-	public void setPopulation(int population) {
-		this.population = population;
-	}
+	public int getPopulation() { return population; }
 
-	public double getExtension() {
-		return extension;
-	}
+	public void setPopulation(int population) { this.population = population; }
 
-	public void setExtension(double extension) {
-		this.extension = extension;
-	}
+	public double getExtension() { return extension; }
 
-	public GovType getGovType() {
-		return govType;
-	}
+	public void setExtension(double extension) { this.extension = extension; }
 
-	public void setGovType(GovType govType) {
-		this.govType = govType;
-	}
+	public GovType getGovType() { return govType; }
 
-	public ArrayList<War> getActiveWars() {
-		return activeWars;
-	}
+	public void setGovType(GovType govType) { this.govType = govType; }
 
-	public void setActiveWars(ArrayList<War> activeWars) {
-		this.activeWars = activeWars;
-	}
+	public ArrayList<War> getActiveWars() { return activeWars; }
+
+	public void setActiveWars(ArrayList<War> activeWars) { this.activeWars = activeWars; }
 }
--- src/main/java/com/example/project/view/DisplayCountries.java
+++ src/main/java/com/example/project/view/DisplayCountries.java
@@ -9,20 +9,25 @@ import java.io.Serializable;
 
 @Named
 @SessionScoped
-public class DisplayCountries implements Serializable {
+public class DisplayCountries implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
+	private Country currCountry; // country focus for further creation, reading and update.
+	private static long nextId;
+	private String errorMessage;
 	private ArrayList<Country> countries;
 	
 	public DisplayCountries() {
+		currCountry = new Country();
+		nextId = 0;
+		errorMessage = "No error messages.";
 		countries = new ArrayList<>();
-		this.countries.add(new Country("alpha", 1000, 1000.0, GovType.Anarchism,      this.getActiveWars(3)));
-		this.countries.add(new Country("beta",  2000, 2000.0, GovType.AnarchoCapitalism, this.getActiveWars(3)));
-		this.countries.add(new Country("gamma", 3000, 3000.0, GovType.Capracracy,     this.getActiveWars(3)));
-		this.countries.add(new Country("delta", 4000, 4000.0, GovType.Communism,      this.getActiveWars(3)));
-		this.countries.add(new Country("tetha", 5000, 5000.0, GovType.Demarchy,       this.getActiveWars(3)));
-		this.countries.add(new Country("rho",   6000, 6000.0, GovType.Corporatocracy, this.getActiveWars(3)));
-		System.out.println("sample data added!");
+		this.countries.add(new Country(getNextId(), "alpha", 1000, 1000.0, GovType.Anarchism,      this.getActiveWars(3)));
+		this.countries.add(new Country(getNextId(), "beta",  2000, 2000.0, GovType.AnarchoCapitalism, this.getActiveWars(3)));
+		this.countries.add(new Country(getNextId(), "gamma", 3000, 3000.0, GovType.Capracracy,     this.getActiveWars(3)));
+		this.countries.add(new Country(getNextId(), "delta", 4000, 4000.0, GovType.Communism,      this.getActiveWars(3)));
+		this.countries.add(new Country(getNextId(), "tetha", 5000, 5000.0, GovType.Demarchy,       this.getActiveWars(3)));
+		this.countries.add(new Country(getNextId(), "rho",   6000, 6000.0, GovType.Corporatocracy, this.getActiveWars(3)));
 	}
 	
 	private ArrayList<War> getActiveWars(int number) {
@@ -33,12 +38,60 @@ public class DisplayCountries implements Serializable {
 		return activeWars;
 	}
 
-	public ArrayList<Country> getCountries() {
-		return countries;
+	public ArrayList<Country> getCountries() { return countries; }
+		
+	public Country getCurrCountry() { return currCountry; }
+
+	public void setCurrCountry(Country currCountry) { this.currCountry = currCountry; }
+	
+	public String getErrorMessage() { return errorMessage; }
+	
+	public static long getNextId() { return nextId++; }
+
+	// ------------------------------------------------------
+	// CRUD
+	// ------------------------------------------------------
+	public String createCountry() {
+		this.currCountry = new Country();
+		return "/editCountry.xhtml";	
 	}
 	
-	public String editCountry(Country c) {
-		System.out.println(c.getName());
-		return "/editCountry.xhtml?faces-redirect=true";
+	public void readCountry() {
+		// TO DO
+	}
+	
+	public String updateCountry(Country c) {
+		this.currCountry = c;
+		return "/editCountry.xhtml";
+	}
+	
+	public void deleteCountry(Country c) {
+		countries.remove(c);
+	}
+	
+	public String saveCountry() {
+		int idx = countries.indexOf(currCountry);
+		System.out.println(currCountry.getName());
+		if(idx != -1)
+			countries.set(idx, currCountry);
+		else
+			countries.add(currCountry);
+		return "/index.xhtml";
+	}
+	// ------------------------------------------------------
+	// MISC
+	// ------------------------------------------------------
+	public Country getCountry(long id) {
+		for(Country c : countries)
+			if(c.getId() == id) 
+				return c;
+		return null;
+	}
+	
+	public boolean isNameAvailable(String name) {
+		for(Country c : countries)
+			if(c.getName().equals(name))
+				return false;
+		return true;
 	}
 }
